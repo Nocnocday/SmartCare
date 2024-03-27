20c7884ae1a760c872f5a6499cf99833195672e3
Nhu Ngoc <ngoc0935518742@gmail.com>
2024-03-27 20:16:41 +0700

    change file

---
+++ smartcare/src/pages/admin/index.js
@@ -0,0 +1,4 @@
+export { default as AdminLayout } from './AdminLayout'
+export { default as CreateAccount } from './CreateAccount'
+export { default as ManageStudent } from './ManageStudent'
+export { default as ManageTeacher } from './ManageTeacher'
---
+++ smartcare/src/utils/path.js
@@ -0,0 +1,25 @@
+export const pathAdmin = {
+    // Admin
+    ADMIN: "admin",
+    CREATE_ACCOUNT: "create-account",
+    MANAGE_STUDENT: "manage-students",
+    MANAGE_TEACHER: "manage-teacher",
+    STUDENTDETAIL: "studentdetail",
+    STUDENTDETAIL_UID: "studentdetail/:uid",
+};
+
+export const pathAssisstant = {
+    // Member
+    ASSISSTANT: "assisstant",
+    MANAGE_SCHEDULE: "manage-schedule",
+    MANAGE_FEE: "manage-fee",
+   
+};
+
+export const common = {
+    LOGIN: "login",
+    DASHBOARD: "dashboard",
+    PROFILE: "profile",
+    PROFILE_UID: "profile/:uid",
+    FORGOTPASSWORD: "forgot",
+};
